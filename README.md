f37f4f027ef6d95d30ee11bd9bea026144ffbf95
Eric Fried <efried@redhat.com>
2023-08-13 14:12:54 -0500

    README, prettier printing, black

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # hiit
 Scripts for HIIT (high-intensity interval training) workouts
+
+## hiit.py
+
+Just run it.
+(You gotta have [python](https://www.python.org/downloads/).)
+It'll generate a random workout cycle.
+It will avoid placing two exercises in sequence if they work the same body part (according to the currently-hardcoded data).
+
+```
+[efried@efried hiit]$ ./hiit.py
+Cross body hammer curl
+Deadlifts bent
+Ball toss/lunge
+Overhead triceps
+Battle Rope
+Sissy squats
+Upright row
+Plank
+Dumbbell bent over fly
+Deadlifts bent
+Single Leg Raise
+```
+
+### Customizing
+Edit the script to customize
+- `ex_per_cycle`: Tweak the number of exercises per cycle.
+- `exercises`: Add or remove exercises; adjust which muscle groups are worked for each.
--- hiit.py
+++ hiit.py
@@ -5,191 +5,219 @@ import random
 ex_per_cycle = 11
 
 exercises = {
-"Overhead triceps": set((
-    "triceps",
-)),
-"DB Curls": set((
-    "biceps",
-)),
-"Arnold Press": set((
-    "deltoids",
-)),
-"Wall Ball": set((
-    "triceps",
-    "pecs",
-    "cardio",
-    "lumbar",
-)),
-"Overhead Ball Slams": set((
-    "lats",
-    "cardio",
-    "quads",
-)),
-"Upright row": set((
-    "biceps",
-    "deltoids",
-)),
-"Dumbbell bent over fly": set((
-    "deltoids",
-    "rhomboids",
-)),
-"Bent over row": set((
-    "lats",
-    "biceps",
-)),
-"Renegade Row": set((
-    "biceps",
-    "lats",
-    "triceps",
-    "pecs",
-)),
-"Lateral Ball Slam": set((
-    "triceps",
-    "pecs",
-    "core",
-    "cardio",
-    "lumbar",
-    "quads",
-)),
-"Cross body hammer curl": set((
-    "biceps",
-)),
-"Zottman curl": set((
-    "biceps",
-)),
-"Scaption": set((
-    "deltoids",
-)),
-"Handstand": set((
-    "triceps",
-    "deltoids",
-    "trapezius",
-)),
-"Squats": set((
-    "quads",
-    "glutes",
-)),
-"180 squats": set((
-    "quads",
-    "glutes",
-)),
-"DB reverse Lunge": set((
-    "quads",
-    "adductors",
-)),
-"Lunges": set((
-    "quads",
-    "adductors",
-)),
-"Walking lunges": set((
-    "quads",
-    "adductors",
-)),
-"Frog jumps": set((
-    "quads",
-    "cardio",
-    "glutes",
-)),
-"Rover’s Revenge": set((
-    "abductors",
-)),
-"Wall Sit": set((
-    "quads",
-)),
-"Deadlifts bent": set((
-    "lumbar",
-)),
-"Deadlifts straight": set((
-    "hamstrings",
-    "lumbar",
-)),
-"Sissy squats": set((
-    "quads",
-)),
-"Heel Flicks": set((
-    "cardio",
-    "hamstrings",
-)),
-"High Knees": set((
-    "hip flexors",
-    "cardio",
-    "calves",
-)),
-"Jumping Jacks": set((
-    "cardio",
-    "calves",
-)),
-"Jumprope": set((
-    "cardio",
-    "calves",
-)),
-"Russian Twist": set((
-    "core",
-)),
-"Bird Dog": set((
-    "deltoids",
-    "glutes",
-)),
-"Battle Rope": set((
-    "cardio",
-)),
-"Bag Kicks": set((
-    "cardio",
-    "core",
-    "hip flexors",
-)),
-"Bear Crawl": set((
-    "cardio",
-    "triceps",
-    "deltoids",
-)),
-"Glute Bridge": set((
-    "glutes",
-)),
-"Plank": set((
-    "core",
-)),
-"Comando Push-ups": set((
-    "core",
-    "triceps",
-    "deltoids",
-    "pecs",
-)),
-"Superman": set((
-    "rhomboids",
-    "glutes",
-)),
-"Single Leg Raise": set((
-    "hamstrings",
-)),
-"Mountain Climbers": set((
-    "cardio",
-    "hip flexors",
-    "deltoids",
-    "triceps",
-)),
-"Around the World": set((
-    "triceps",
-    "deltoids",
-)),
-"Kettlebell Swing": set((
-    "cardio",
-    "lumbar",
-    "deltoids",
-)),
-"Burpees": set((
-    "cardio",
-    "quads",
-    "hip flexors",
-    "triceps",
-    "pecs",
-)),
-"Ball toss/lunge": set((
-    "pecs",
-    "cardio",
-    "quads",
-    "glutes",
-)),
+    "Overhead triceps": set(("triceps",)),
+    "DB Curls": set(("biceps",)),
+    "Arnold Press": set(("deltoids",)),
+    "Wall Ball": set(
+        (
+            "triceps",
+            "pecs",
+            "cardio",
+            "lumbar",
+        )
+    ),
+    "Overhead Ball Slams": set(
+        (
+            "lats",
+            "cardio",
+            "quads",
+        )
+    ),
+    "Upright row": set(
+        (
+            "biceps",
+            "deltoids",
+        )
+    ),
+    "Dumbbell bent over fly": set(
+        (
+            "deltoids",
+            "rhomboids",
+        )
+    ),
+    "Bent over row": set(
+        (
+            "lats",
+            "biceps",
+        )
+    ),
+    "Renegade Row": set(
+        (
+            "biceps",
+            "lats",
+            "triceps",
+            "pecs",
+        )
+    ),
+    "Lateral Ball Slam": set(
+        (
+            "triceps",
+            "pecs",
+            "core",
+            "cardio",
+            "lumbar",
+            "quads",
+        )
+    ),
+    "Cross body hammer curl": set(("biceps",)),
+    "Zottman curl": set(("biceps",)),
+    "Scaption": set(("deltoids",)),
+    "Handstand": set(
+        (
+            "triceps",
+            "deltoids",
+            "trapezius",
+        )
+    ),
+    "Squats": set(
+        (
+            "quads",
+            "glutes",
+        )
+    ),
+    "180 squats": set(
+        (
+            "quads",
+            "glutes",
+        )
+    ),
+    "DB reverse Lunge": set(
+        (
+            "quads",
+            "adductors",
+        )
+    ),
+    "Lunges": set(
+        (
+            "quads",
+            "adductors",
+        )
+    ),
+    "Walking lunges": set(
+        (
+            "quads",
+            "adductors",
+        )
+    ),
+    "Frog jumps": set(
+        (
+            "quads",
+            "cardio",
+            "glutes",
+        )
+    ),
+    "Rover’s Revenge": set(("abductors",)),
+    "Wall Sit": set(("quads",)),
+    "Deadlifts bent": set(("lumbar",)),
+    "Deadlifts straight": set(
+        (
+            "hamstrings",
+            "lumbar",
+        )
+    ),
+    "Sissy squats": set(("quads",)),
+    "Heel Flicks": set(
+        (
+            "cardio",
+            "hamstrings",
+        )
+    ),
+    "High Knees": set(
+        (
+            "hip flexors",
+            "cardio",
+            "calves",
+        )
+    ),
+    "Jumping Jacks": set(
+        (
+            "cardio",
+            "calves",
+        )
+    ),
+    "Jumprope": set(
+        (
+            "cardio",
+            "calves",
+        )
+    ),
+    "Russian Twist": set(("core",)),
+    "Bird Dog": set(
+        (
+            "deltoids",
+            "glutes",
+        )
+    ),
+    "Battle Rope": set(("cardio",)),
+    "Bag Kicks": set(
+        (
+            "cardio",
+            "core",
+            "hip flexors",
+        )
+    ),
+    "Bear Crawl": set(
+        (
+            "cardio",
+            "triceps",
+            "deltoids",
+        )
+    ),
+    "Glute Bridge": set(("glutes",)),
+    "Plank": set(("core",)),
+    "Comando Push-ups": set(
+        (
+            "core",
+            "triceps",
+            "deltoids",
+            "pecs",
+        )
+    ),
+    "Superman": set(
+        (
+            "rhomboids",
+            "glutes",
+        )
+    ),
+    "Single Leg Raise": set(("hamstrings",)),
+    "Mountain Climbers": set(
+        (
+            "cardio",
+            "hip flexors",
+            "deltoids",
+            "triceps",
+        )
+    ),
+    "Around the World": set(
+        (
+            "triceps",
+            "deltoids",
+        )
+    ),
+    "Kettlebell Swing": set(
+        (
+            "cardio",
+            "lumbar",
+            "deltoids",
+        )
+    ),
+    "Burpees": set(
+        (
+            "cardio",
+            "quads",
+            "hip flexors",
+            "triceps",
+            "pecs",
+        )
+    ),
+    "Ball toss/lunge": set(
+        (
+            "pecs",
+            "cardio",
+            "quads",
+            "glutes",
+        )
+    ),
 }
 
 workout = []
@@ -197,11 +225,15 @@ seen_ex = set()
 last_groups = set()
 for _ in range(ex_per_cycle):
     # TODO: Emergency brake
-    remaining = [k for k, v in exercises.items() if k not in seen_ex and not v.intersection(last_groups)]
+    remaining = [
+        k
+        for k, v in exercises.items()
+        if k not in seen_ex and not v.intersection(last_groups)
+    ]
     if len(remaining) == 0:
         raise RuntimeError("Ran out of exercises!")
     ex = random.choice(remaining)
     last_groups = exercises[ex]
     workout.append(ex)
 
-print(workout)
+print("\n".join(workout))
